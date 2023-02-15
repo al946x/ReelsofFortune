2120a7cdbee8df8ccd15171a833c241031fb18b4
Alina Ali01~ <alina.zenam@hotmail.com>
2023-02-15 20:44:51 +0000

    appjs imports done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import Header from './components/Header';
+import Search from './components/Search';
+import Sidebar from './components/Sidebar';
 
-
-
-
-
-
-
-
-
-
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      <Sidebar />
+    </div>
+  );
+}
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <header>
+            <h1>Reels of Fortune</h1>
+        </header>
+    );
+
+}
+
+export default Header;
