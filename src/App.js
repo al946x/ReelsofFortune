3b68f59a745e423c204e9146c3801ab9837f81e6
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-02-21 19:06:09 +0000

    fixed errors and got the app working again

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
+import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import MovieList from "./components/MovieList";
-import ListHeaders from "./components/ListHeaders";
-import Search from "./components/Search";
 import Header from "./components/Header";
 import Favourites from "./pages/Favourites";
 import Contact from "./pages/Contact";
-import RandomMovie from "./components/RandomMovie";
+import Home from "./pages/Home";
 
 function App() {
-  const [movies, setMovies] = useState();
   
-  // const [searchMovie, setSearchMovie] = useState('');
-  const [randomMovie, setRandomMovie] = useState({});
-
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=starwars&apikey=91ef3dfe";
-
-    const response = await fetch(url);
-    const responseJson = await response.json();
-
-
-    
-    setMovies(responseJson);
-    console.log(responseJson);
-
-    const newRandomMovie =
-      responseJson.Search[Math.floor(Math.random() * responseJson.Search.length)];
-
-      
-    
-    setRandomMovie(newRandomMovie);
-    console.log(randomMovie);
-  };
-
-  useEffect(() => {
-    getMovieRequest();
-  }, []);
 
   return (
     <div className="container-fluid movie-app">
@@ -48,14 +18,13 @@ function App() {
       </div>
       <div className="row">
       <Routes>
-        <Route path="/" element={<App />}>
-          <RandomMovie movies={movies} randomMovie={randomMovie} movieRequest={getMovieRequest} />
-        </Route>
+        <Route path="/" element={<Home />}></Route>
 
         <Route path="/favourites" element={<Favourites />}></Route>
 
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
+
 
       </div>
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,14 +3,15 @@ import React from 'react';
 const MovieList = (props) =>{
     console.log(props.movie);
     return(
-        <>
-         {props.movies.Search.map((movie, index)=>
-          <div key={index} className='d-flex justify-content-start'>
+        
+         
+        <div className='d-flex justify-content-start'>
             <h3>{props.movieTitle}</h3>
             <img src= {props.poster} alt='movie'></img>
             <p>{props.year}</p>
-         </div>)}
-        </>
+        </div>
+        
+        
     )
 }
 
